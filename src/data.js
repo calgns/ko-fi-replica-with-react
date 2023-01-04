589927d5470432e79c10643d98fc7ae5e708ae80
Matheus <matheus110903@gmail.com>
2023-01-04 11:50:55 -0300

    add lista de objetos a serem utilizados pelos componentes

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const posts = [
+  {title: "Ko-fimas Day 1: Win a Ko-fi Mug! ", texts:["Introducing 12 Days of Ko-fimas! There are 12 days of festive prizes to be won, and we’ll be revealing a new prize every day", "For day 1, you could win your own Ko-fi mug IRL! 🙌 ", "To enter: Share a post on social promoting your Ko‑fi page using #Kofimas ", "Prize: 1 x Limited Edition Ko-fi Mug"], id: 1, cover: "https://storage.ko-fi.com/cdn/useruploads/post/f0f7e57b-1ed9-457c-b7ab-de38a06df4ee_1.jpg"},
+  {title: "Ko-fimas Day 2: Win a $50 Tip! ", texts:["We’re sending 3 lucky Ko-fi creators $50 each 🙌", " To enter: Share a post on social promoting your Ko‑fi page using #Kofimas.", "Prize: 3 x $50 cash prizes", "Full challenge info: ko-fi.com/post/-12-Days-of-Ko-fimas-O5O4GQIRY"], id: 2, cover: "https://storage.ko-fi.com/cdn/useruploads/post/b8a9df31-dfb5-4060-82da-6f0fb88b184b_2.jpg"},
+  {title: "Ko-fimas Day 3: Festive Wallpapers ", "texts":["Head to our Ko-fi Shop to download a pack of free festive wallpapers, all designed by our incredible Ko-fi ambassadors!", '😎'], id: 3, cover: "https://storage.ko-fi.com/cdn/useruploads/post/0002019d-3fc7-46e9-a109-e0b796938d65_3.jpg"},
+];
+
+export const featuredPosts = [
+  {img: "https://storage.ko-fi.com/cdn/useruploads/display/b64e8b3c-b2a0-4030-bd42-9cf5a049c8b1_giveawaycopy.jpg", text: "✨ Lorem, ipsum dolor.", id: "l1"},
+  {img: "https://storage.ko-fi.com/cdn/useruploads/post/b8a9df31-dfb5-4060-82da-6f0fb88b184b_2.jpg", text: "✨ Lorem ipsum dolor sit amet.", id: "r2"},
+  {img: "https://storage.ko-fi.com/cdn/useruploads/post/0002019d-3fc7-46e9-a109-e0b796938d65_3.jpg", text: "✨ Lorem ipsum dolor sit.", id: "r1"},
+  {img: "https://storage.ko-fi.com/cdn/useruploads/post/f0f7e57b-1ed9-457c-b7ab-de38a06df4ee_1.jpg", text: "✨ Lorem ipsum dolor sit amet consectetur.", id: "LF11"},
+  {img: "https://storage.ko-fi.com/cdn/useruploads/post/b8a9df31-dfb5-4060-82da-6f0fb88b184b_2.jpg", text: "✨ Lorem ipsum dolor sit amet consectetur.", id: "47b"},
+  {img: "https://storage.ko-fi.com/cdn/useruploads/post/0002019d-3fc7-46e9-a109-e0b796938d65_3.jpg", text: "✨ Lorem ipsum dolor sit amet consectetur.", id: "55a"},
+  {img: "https://storage.ko-fi.com/cdn/useruploads/post/f0f7e57b-1ed9-457c-b7ab-de38a06df4ee_1.jpg", text: "✨ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius.", id: "l4l"},
+  {img: "https://storage.ko-fi.com/cdn/useruploads/post/b8a9df31-dfb5-4060-82da-6f0fb88b184b_2.jpg", text: "✨ Lorem ipsum dolor sit amet consectetur.", id: "d4c"},
+  {img: "https://storage.ko-fi.com/cdn/useruploads/post/0002019d-3fc7-46e9-a109-e0b796938d65_3.jpg", text: "✨ Lorem ipsum dolor sit amet consectetur.", id: "H32"},
+];
+
+export const navBlog = [
+  {link: "Explore", direction:"#", mob: ""},
+  {link: "About", direction:"/Blog", mob: ""},
+  // {link: "Gold", direction:"#", mob: "extra"},
+  // {link: "Blog", direction:"/Blog", mob: "extra"},
+];
+
+export const galleryPosts = [
+  {title: "Loem boemia bohemian", cover: "https://placeimg.com/301/300/animals", k:"1!a"},
+  {title: "Loem boemia bohemian casparovi", cover: "https://placeimg.com/301/200/animals/grayscale", k:"b!5"},
+  {title: "Loem boemia bohemian casparovi senna", cover: "https://placeimg.com/301/150/animals/sepia", k:"a!3"},
+  {title: "Loem boemia bohemian casparovi senna cruicscrevielie varsalis", cover: "https://placehold.co/600x400/orange/white?text=You+are+with+me+or+againt+me&font=raleway", k:"a!7"},
+  {title: "Loem boemia bohemian", cover: "https://placeimg.com/301/150/animals", k:"h1!n1"},
+  {title: "Loem boemia bohemian casparovi senna cruicscrevielie varsalis", cover: "https://placeimg.com/451/150/animals/grayscale", k:"k!4"},
+  {title: "Loem boemia bohemian casparovi", cover: "https://placeimg.com/301/100/animals/sepia", k:"h6!"},
+  {title: "Loem boemia bohemian casparovi senna", cover: "https://placeimg.com/301/150/animals/grayscale", k:"co!v"},
+  {title: "Loem boemia bohemian casparovi senna cruicscrevielie varsalis", cover: "https://place-hold.it/50x50", k:"flo!90"},
+  {title: "Loem boemia bohemian casparovi", cover: "https://place-hold.it/500x50", k:"h6!75"},
+  {title: "Loem boemia bohemian casparovi senna", cover: "https://place-hold.it/300x600", k:"co!vir"},
+  {title: "Loem boemia bohemian casparovi senna cruicscrevielie varsalis", cover: "https://place-hold.it/600x300", k:"flo0!96"},
+  {title: "Loem boemia bohemian casparovi senna cruicscrevielie varsalis", cover: "https://placehold.co/300x300/255254203/6e05ff", k:"flo0!96"},
+  {title: "Loem boemia bohemian casparovi senna cruicscrevielie varsalis", cover: "https://placehold.co/300x300/cyan/teal", k:"flo0!96"},
+  {title: "Loem boemia bohemian casparovi senna cruicscrevielie varsalis", cover: "https://placehold.co/300x300/lime/green", k:"flo0!96"},
+  {title: "Loem boemia bohemian casparovi senna cruicscrevielie varsalis", cover: "https://placehold.co/300x150?text=moi+plaqueti", k:"flo0!96"},
+  {title: "Loem boemia bohemian casparovi senna cruicscrevielie varsalis", cover: "https://placehold.co/300x150/090809/f40000", k:"flo0!96"},
+];
+
+export const shopItems = [
+  { type: "arch",  price: "Free", color: "bg-pink-600",  title: "Loem bohea darreia pancreia", details: "lorem ipsum amadir est cist nalum", category: "arch"},
+  { type: "animals",  price: "R$ 200 +", color: "bg-fuchsia-600",  title: "Loemim diareia pancreas", details: "lobinho lobo lobinho ovelga ovelha", category: "animals"},
+  { type: "tech",  price: "$500 +", color: "bg-emerald-500",  title: "fogo no babilonia pega fooogooo na bab i", details: "fogo na babilonia sct ig babilonus", category: "tech"},
+
+  { type: "nature/sepia",  price: "$8000 +", color: "bg-amber-600",  title: "Loem bohea darreia pancreia", details: "lorem ipsum amadir est cist nalum", category: "nature"},
+  { type: "people/sepia",  price: "R$ 150 -", color: "bg-teal-600",  title: "Loemim diareia pancreas", details: "lobinho lobo lobinho ovelga ovelha", category: "people"},
+  { type: "tech/sepia",  price: "$???", color: "bg-sky-500",  title: "fogo no babilonia pega fooogooo na bab i", details: "fogo na babilonia sct ig babilonus", category: "tech"},
+
+  { type: "arch/grayscale",  price: "Free", color: "bg-pink-600",  title: "Loem bohea darreia pancreia", details: "lorem ipsum amadir est cist nalum", category: "arch"},
+  { type: "animals/grayscale",  price: "R$ 200 +", color: "bg-fuchsia-600",  title: "Loemim diareia pancreas", details: "lobinho lobo lobinho ovelga ovelha", category: "animals"},
+  { type: "tech/grayscale",  price: "$900 +", color: "bg-emerald-500",  title: "fogo no babilonia pega fooogooo na bab i", details: "fogo na babilonia sct ig babilonus", category: "tech"},
+
+  { type: "arch/sepia",  price: "$ 758000 +", color: "bg-amber-600",  title: "Loem bohea darreia pancreia", details: "lorem ipsum amadir est cist nalum", category: "arch"},
+  { type: "animals/grayscale",  price: "R$ 600 -", color: "bg-teal-600",  title: "Loemim diareia pancreas", details: "lobinho lobo lobinho ovelga ovelha", category: "animals"},
+  { type: "tech/sepia",  price: "$1000", color: "bg-sky-500",  title: "fogo no babilonia pega fooogooo na bab i", details: "fogo na babilonia sct ig babilonus", category: "tech"},
+]
