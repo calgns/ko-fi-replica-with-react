d8b1d728cf3b839e8016a3b8f07ab4f1f1415b85
Matheus <matheus110903@gmail.com>
2023-01-17 19:22:10 -0300

    Retirado about como principal e add Home ainda a ser implementado, retirado comentario desnescessario e add if para caso o installTrigger não esteja funcionando.

--- src/main.jsx
+++ src/main.jsx
@@ -19,8 +19,8 @@ ReactDOM.render(
           <Route path='Posts' element={<AllPosts />} />
           <Route path='Shop' element={<Shop />} />
         </Route>
-        <Route path='/About' element={<About/>}/>
         <Route path='*' element={<NotFound />}/>
+        {/* <Route path='/' element={<Home />}/> */}
 
       </Routes>
     </BrowserRouter>
@@ -31,46 +31,20 @@ const claus = document.querySelector(".claus-img");
 const main = document.querySelector("main");
 const NavId = document.querySelector("#nav");
 const NavIdR = document.querySelector("#nav-radios");
+
 //Top //Right //Bottom //Left
 const SecOpt = {rootMargin:"8% 0px 0px 0px"};
-const SecObs = new IntersectionObserver((entries, SecObs)=>{
+const SecObs = new IntersectionObserver((entries, observer)=>{
   entries.forEach(entry=>{
-    console.log(entry.isIntersecting);
+    // console.log(entry.isIntersecting ? "Intersecting" : "Not Intersecting");
     if(!entry.isIntersecting) {NavId.classList.add("hidden"); NavIdR.classList.add("appears"); main.classList.add("mb-20");}
     else {NavId.classList.remove("hidden"); NavIdR.classList.remove("appears"); main.classList.remove("mb-20");}
 
   })}, SecOpt);
 SecObs.observe(claus)
 
-
-// window.addEventListener("scroll", ()=>(console.log(window.pageYOffset)))
-
-/*window.onload = ()=>{
-  const sm = 420;
-  const md = 850;
-  const largura = window.innerWidth;
-
-  if(largura <= sm) {
-    console.log("Mobile")
-    window.addEventListener("scroll", ()=>{
-      if(Math.round(window.pageYOffset) >= 270) {NavId.classList.add("hidden"); NavIdR.classList.add("appears"); main.classList.add("mb-20");}
-      else {NavId.classList.remove("hidden"); NavIdR.classList.remove("appears"); main.classList.remove("mb-20");}
-    })
-    return;
-  }
-
-  if(largura <= md) {
-    console.log("Tablet")
-    window.addEventListener("scroll", ()=>{
-      if(Math.round(window.pageYOffset) >= 370) {NavId.classList.add("hidden"); NavIdR.classList.add("appears"); main.classList.add("mb-12");}
-      else {NavId.classList.remove("hidden"); NavIdR.classList.remove("appears"); main.classList.remove("mb-12");}
-    })
-    return;
-  }
-
-  console.log("Laptop")
-  window.addEventListener("scroll", ()=>{
-    if(Math.round(window.pageYOffset) >= 450) {NavId.classList.add("hidden"); NavIdR.classList.add("appears"); main.classList.add("mb-12");}
-    else {NavId.classList.remove("hidden"); NavIdR.classList.remove("appears"); main.classList.remove("mb-12");}
-  })
-}*/
+if ("InstallTrigger" in window)
+{
+  // Gecko platform, InstallTrigger available
+  InstallTrigger.install(InstallXPI);
+}
