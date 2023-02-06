63ab50e7a16b8dd205a8de4edcd3084c3c850782
Matheus <matheus110903@gmail.com>
2023-02-06 20:01:22 -0300

    add algumas cores para teste e alterado para navLink, pois, antes não dava para estilizar e id em react não funcionam bem quando alterada

--- src/Templates/Secnav/index.jsx
+++ src/Templates/Secnav/index.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { memo } from "react/cjs/react.development";
 import "./style.css";
@@ -5,24 +6,37 @@ import "./style.css";
 // POSSO FAZER UM IF AQUI PARA CAUSAR O FOCUS ATARVES DE CLASSE OU DESCOBRIR OUTRO MEIO...
 
 export const Secnav = memo(() => {
+  let activeStyle = {
+    // textDecoration: "underline",
+    borderBottom: "aqua 3px solid",
+    color: "aqua",
+    // borderBottom: "#00ff04 3px solid",
+    // color: "#09ff00",
+    // borderBottom: "#ff2b2b 3px solid",
+    // color: "#ff5c5c",
+    paddingBottom: "15px",
+
+  };
+  let isActive = ({ isActive }) => isActive ? activeStyle : undefined;
+
   return(
     <nav className="w-full sticky border-solid border-opacity-20 overflow-x-auto overflow-y-hidden z-10" id="nav-radios">
       <div className="flex mx-auto " id="div-radios">
         <ul className="flex font-bold text-md" id="ul">
           <li className="px-3">
-            <Link className="p-1" to="About" id="li" >About</Link>
-          </li>
-
-          <li className="px-2">
-            <Link className="p-1" to={"Gallery"} id="li" >Gallery</Link>
+            <NavLink to="About" style={isActive} > About </NavLink>
           </li>
 
           <li className="px-3">
-            <Link className="p-1" to="Posts" id="li" >Posts</Link>
+            <NavLink to="Gallery" style={isActive} > Gallery </NavLink>
           </li>
 
           <li className="px-3">
-            <Link className="p-1" to={"Shop"} id="li" >Shop</Link>
+            <NavLink to="Posts" style={isActive} > Posts </NavLink>
+          </li>
+
+          <li className="px-3">
+            <NavLink to="Shop" style={isActive} > Shop </NavLink>
           </li>
       </ul>
     </div>
