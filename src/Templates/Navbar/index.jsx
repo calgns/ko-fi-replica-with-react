dc2e0b6f7330fc58ed0a3c3894f15566ec71a72e
Matheus <matheus110903@gmail.com>
2023-02-06 20:29:20 -0300

    add pop para cadastro

--- src/Templates/Navbar/index.jsx
+++ src/Templates/Navbar/index.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
-import { memo } from "react/cjs/react.development";
+import { memo, useEffect, useState } from "react/cjs/react.development";
 import { NavLink } from "../../Components/NavLink";
+import { SingUp } from "../../Components/SingUp";
+import { PopUp } from "../Pop-up";
 import "./style.css";
 
 
@@ -11,30 +13,47 @@ function toggleMenu() {
 
 export const NavigationBar = memo( ({LinksList, buttonClass="default-btn", buttonText="Sign up" }) => {
   const currentUrl = window.location.pathname.toLowerCase()
+  const [popUp, setPopUp] = useState(false)
+  const [timedPopUp, setimedPopUp] = useState(false)
+
+  useEffect(()=>{
+    setTimeout(()=>{setimedPopUp(true)}, 3000)
+  }, [])
 
   return (
-  <nav id="nav" className="fixed w-full top-0 shadow-xl z-20">
-    <div className="navbar justify-between max-w-3xl lg:max-w-6xl  m-auto flex p-3 ">
-      <div className="logo mt-1 -ml-1">
-        <Link to="/">
-          {(currentUrl === "/"
-            ? <img src="https://storage.ko-fi.com/cdn/Kofi_Logo_Blue.svg" className="w-24" alt="" /> 
-            : <img src="https://storage.ko-fi.com/cdn/brandasset/logo_white_stroke_small.png" className="w-24" alt="" /> )}
-        </Link>
-      </div>
-      <div className="flex items-center">
-        {( currentUrl === "/" ? <a href="" className="log-in-mob font-bold mr-2 p-2 text-wheat sm:hidden hover:text-indigo-500">Log in</a> : "")}
-        <button className={"left-btn "+buttonClass}>{buttonText}</button>
-        <button className="btn-mobile text-4xl " onClick={toggleMenu}><span className="hamburger"></span></button>
+  <>
+    <nav id="nav" className="fixed w-full top-0 shadow-xl z-20">
+      <div className="navbar justify-between max-w-3xl lg:max-w-6xl  m-auto flex p-3 ">
+        <div className="logo mt-1 -ml-1">
+          <Link to="/">
+            {(currentUrl === "/"
+              ? <img src="https://storage.ko-fi.com/cdn/Kofi_Logo_Blue.svg" className="w-24" alt="" /> 
+              : <img src="https://storage.ko-fi.com/cdn/brandasset/logo_white_stroke_small.png" className="w-24" alt="" /> )}
+          </Link>
+        </div>
+        <div className="flex items-center">
+          {( currentUrl === "/" ? <a href=""  onClick={()=>setPopUp(true)} className="log-in-mob font-bold mr-2 p-2 text-wheat sm:hidden hover:text-indigo-500">Log in</a> : "")}
+          <button className={"left-btn "+buttonClass} onClick={()=>setPopUp(true)}>{buttonText}</button>
+          <button className="btn-mobile text-4xl " onClick={toggleMenu}><span className="hamburger"></span></button>
 
-        <ul className="menu flex font-bold items-center">
-          {LinksList.map(a=>(<NavLink link={a.link} direction={a.direction} mob={a.mob} key={a.link + a.mob}/>))} 
-          <li className={"mx-4 text-base "}><a href="#">Log In</a></li>
-        </ul>
-        <button className={buttonClass}>{buttonText}</button>
-        
+          <ul className="menu flex font-bold items-center">
+            {LinksList.map(a=>(<NavLink link={a.link} direction={a.direction} mob={a.mob} key={a.link + a.mob}/>))} 
+            <li className={"mx-4 text-base "} onClick={()=>setPopUp(true)}><a href="#">Log In</a></li>
+          </ul>
+          <button onClick={()=>setPopUp(true)} className={buttonClass}>{buttonText}</button>
+        </div>
       </div>
-    </div>
-  </nav>);
+    </nav>
+    
+    <PopUp trigger={popUp} setTrigger={setPopUp}>
+      <SingUp />
+    </PopUp>
+
+    {/* <PopUp trigger={timedPopUp} setTrigger={setimedPopUp}>
+      <h3 className="font-bold text-xl">Meu pop up Temporal</h3>
+      <p>Chamado atravez do tempo(Cronometro)</p>
+    </PopUp> */}
+  </>
+  );
 });
 
