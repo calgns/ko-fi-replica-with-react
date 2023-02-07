dd9e4e871147936e4b5fb7b4634bcbf1993f595f
Matheus <matheus110903@gmail.com>
2023-02-07 15:47:00 -0300

    componente movido para o mesmo local do loop, pois, é desnescessario o colocar em uma pasta diferente

--- src/Components/gallery-items/index.jsx
+++ src/Components/gallery-items/index.jsx
@@ -1,15 +1,27 @@
-// import { GalleryItem } from "../gallery-item";
 import "./style.css";
 
 export const GalleryItems = ({values})=>(
-  <ul className="w-full max-md:gap-2 mx-auto justify-center items-center grid gap-4 mt-10 ">
+  <ul className="grid-gallery grid w-full max-md:gap-2 mx-auto justify-center items-center  gap-4 mt-10 ">
     {values.map(value=> 
-      <li id="gli">
-        <div className="gallery-item bg-no-repeat bg-cover bg-center rounded-2xl " 
-          style={{backgroundImage: "url("+value.cover+')'}}></div>
-        {/* <img src={cover} alt={title} /> */}
-        <p className="max-md:hidden">{value.title}</p>
-      </li>
+      <Item
+        title={value.title}
+        cover={value.cover}
+        height={value.height}
+        key={value.k}
+        />
       )}
   </ul>
 ); 
+
+const Item = ({height, cover, title}) => {
+  const putClass = height >= 300 ? "grid-span-2" : "";
+
+  return(
+    <li className={"gli "+putClass}>
+      <div className="gallery-item bg-no-repeat bg-cover bg-center rounded-2xl " 
+        style={{backgroundImage: "url("+cover+height+')'}}></div>
+      {/* <img src={cover} alt={title} /> */}
+      <p className="max-md:hidden">{title}</p>
+    </li>
+  )
+}
