850551e3743a664a0d35c197f29c616e3613462f
Matheus <matheus110903@gmail.com>
2023-02-07 15:24:01 -0300

    componente movido para o mesmo local do loop, pois, é desnescessario o colocar em uma pasta diferente

--- src/Components/latestPosts/index.jsx
+++ src/Components/latestPosts/index.jsx
@@ -1,5 +1,5 @@
-import { LatestPost } from "../latestPost";
-import "./style.css";
+
+const meses = ["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez",];
 
 export const LatestPosts = ({ latests }) => (
   <>
@@ -10,3 +10,17 @@ export const LatestPosts = ({ latests }) => (
     <span className="w-full flex"><a href="#" className="mx-auto my-4 underline text-gray-300">See all</a></span>
   </>
 );
+
+const LatestPost = ({ img, text, id }) => (
+  <div className="flex py-2">
+    <img src={img} alt="" className="w-16 h-9 rounded-lg"/>
+    <div className="px-2 items-baseline">
+      <span>{text} && {id}</span>
+      <br />
+      <span className="text-sm text-gray-400">
+        {Math.round(Math.random() * 31)} {meses[Math.round(Math.random() * 11)]}
+      </span>
+    </div>
+  </div>
+);
+
--- src/Components/shop-items/index.jsx
+++ src/Components/shop-items/index.jsx
@@ -1,8 +1,7 @@
-import { ShopItem } from "../shop-item";
 import "./style.css"
 
 export const ShopMerch = ({dataValues}) =>(
-  <div className="cato flex flex-wrap justify-center items-center gap-4 w-9/12 mx-auto mt-3 ">
+  <div className="category-output flex flex-wrap justify-center items-center gap-4 w-9/12 mx-auto mt-3 ">
     {dataValues.map(
       value=>
         <ShopItem 
@@ -14,3 +13,23 @@ export const ShopMerch = ({dataValues}) =>(
           key={value.color+value.type+value.price} />)}
   </div>
 )
+
+
+const ShopItem = ({ type, price, customColor, title, details  }) => (
+  <div className="shop-card mt-4">
+    <div className="shop-img relative">
+      <img className="rounded-t-lg" src={"https://placeimg.com/300/300/" + type } alt="" />
+
+      <div className="flex justify-end w-full absolute right-0 bottom-2 px-3" >
+        <span className={customColor+" font-bold text-xl rounded-xl px-2 py-1"}>{price}</span>
+      </div>
+    </div>
+
+    <div className="shop-detail bg-indigo-900 bg-opacity-20  flex justify-between rounded-b-lg items-start p-4">
+      <div className="flex flex-col items-start justify-start capitalize w-full ">
+        <span className="text-xl font-bold ">{title}</span>
+        <p className="preview-text text-neutral-300">✨ {details}</p>
+      </div>
+    </div>
+  </div>
+);
